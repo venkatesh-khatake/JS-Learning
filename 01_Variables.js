585e8fe7cef9cced3b2d459161778cd63fb8a0f4
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-02-05 16:01:04 +0530

    Initial Update

---
+++ 01_Variables.js
@@ -0,0 +1,33 @@
+const accountId = 12332;
+let accountEmail = "venkatesh@gmail.com";
+var accountPassword = "12345";
+city = "Pune";
+
+// accountId = 5;     // Not Allowed 
+
+accountEmail = "abc@gmail.com";
+accountPassword = "555555";
+city = "PCMC";
+console.log(accountId);
+
+//01.  What is Variable?
+//  Ans : The Variables are like a container to store a different type of data.
+
+// what is let, var, const?
+// Ans : These are the keywords used  for defining variables, each keyword have different scope
+
+// 1. let : 
+//      - block scope
+
+// 2. var : 
+//      - global or functional scope
+//      - can be re-assigned 
+
+// 3. const : 
+//      - block scope.
+//      - must be initialized 
+//      -can't change value
+
+
+
+console.table([accountId,accountEmail,accountPassword,city]);
