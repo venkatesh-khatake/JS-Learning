2617543663830f5d45199398494475b35d0b59da
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-02-07 14:43:50 +0530

    initial Update

--- Operator.js
+++ Operator.js
@@ -25,3 +25,82 @@ console.table([sum , sub, multi, div, mod]);
 // 
 // Example .: 
 
+let a = 5; 
+let b = 9;
+console.log( a > b); // greater than 
+console.log( a < b); // less than 
+console.log( a == b); // equal to 
+console.log(a === b); // strict equal to 
+console.log(a != b); // not equal to 
+console.log(b !== a); // strict not equal to 
+console.log(a >= b); // greater than or equal to 
+console.log(a <= b); // less than or equal to 
+
+// =========================================================================================
+
+// 3. Logical Operator : 
+// Ans : Logical operator are used to combine or invert boolean values.
+//  && , ||, !
+// Example : 
+let age = 18;
+let isMarried = false;
+if(age >= 18 && isMarried == false){    // logical and : both condition must be true
+    console.log("You are eligible for job")
+}
+else{
+    console.log("You are not eligible.");
+}
+
+
+
+if(age >= 18 || isMarried == false){   // logical or : minimum one condition must be true
+    console.log("You are eligible for job")
+}
+else{
+    console.log("You are not eligible.");
+}
+
+// ===========================================================================================
+
+
+
+// 4. Assignment Operator : 
+// Ans : Assignment Operator are used to assign value to variables.
+//  = , += , -= , *= , .
+
+// Example : 
+
+let assign = 45;
+console.log(assign);
+assign += 1;        // add assignment
+console.log(assign);
+assign -= 1;        // sub assignment
+console.log(assign);
+assign *= 2;        // multiplication assignment
+console.log(assign); 
+assign /= 3;        // division assignment
+console.log(assign);
+assign %= 3;        // modulus assignment
+console.log(assign);
+
+//======================================================================================
+
+
+
+// 5. Ternary Operator : 
+// Ans : A ternary operator is a short-hand notation that evaluates a 
+// a condition and returns one of two expressions. it is also known as conditional operator.
+
+// Example : 
+
+let isAdult = (age >= 18)? "Yes" : "No";
+console.log(isAdult)
+
+
+// =====================================================================================
+
+
+// typeof operator : 
+// typeof opreator is used to know the data type of variable.
+
+console.log(typeof(age))
