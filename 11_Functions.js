de6f8de68746499f81f0fb7c888ce021b0cd35fd
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-04-13 11:43:32 +0530

    Initial Update

--- 11_Functions.js
+++ 11_Functions.js
@@ -11,10 +11,10 @@
  
 // For Example : 
 
-function addition(a, b){
-    return a + b;
-}
-console.log(addition(4,3));
+// function addition(a, b){
+//     return a + b;
+// }
+// console.log(addition(4,3));
 
 
 // return Statement : 
@@ -70,5 +70,104 @@ console.log(addition(4,3));
 
 // =========================================
 
-// Create a function that returns the maximum of three numbers
+//  Write a function that returns the square of a number.
+
+function square(num){
+    return num * num;
+}
+
+console.log(square(5));
+
+// Write a function that checks if a number is even or odd.
+
+function EvenOdd(num){
+    if(num % 2 == 0){
+        return "Even";
+    }
+    else{
+        return "Odd";
+    }
+}
+
+console.log(EvenOdd(5));
+
+// Write a function to find the maximum of two numbers.
+
+function maximum(num1, num2){
+    if(num1 > num2){
+        return `${num1} is greater than ${num2}`;
+    }
+    else if(num1 < num2){
+        return `${num2} is greater than ${num1}`;
+    }
+    else if(num1 == num2){
+        return `both are equal`;
+    }
+    else{
+        return 'please enter a number';
+    }
+}
+
+console.log(maximum(5,"ff"))
+
+// Write a function to return the factorial of a number.
+
+function factorial(num){
+    if(num == 0 || num == 1){
+        return 1
+    }
+    else{
+        return num * factorial(num - 1);
+    }
+}
+console.log(factorial(10));
+
+// Write a function to reverse a string.
+function reverseString(str){
+    let newString = "";
+    for(let i = str.length - 1; i >= 0; i--){
+        newString = newString+ str[i];
+    }
+    return newString;
+}
+console.log(reverseString("Venkatesh"));
+
+// Write a function that counts the number of vowels in a string.
+function countVowels(str){
+    let count = 0; 
+    for(let i = 0; i <= str.length; i++){
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels("Tejaswini"));
+
+// Write a function to check if a string is a palindrome.
+function strPalindrome(str){
+    let newStr = "";
+
+    for(let i = str.length - 1; i >= 0; i--){
+        newStr = newStr + str[i];
+    }
+    if(str == newStr){
+        return 'true';
+    }
+    else{
+        return 'false';
+    }
+}
+
+console.log(strPalindrome("madam"));
+
+
+function table(num){
+    for(let i = 1; i <= 10; i++){
+        console.log(`${num} * ${i} = ${num * i}`);
+    }
+}
+table(5)
+
 
