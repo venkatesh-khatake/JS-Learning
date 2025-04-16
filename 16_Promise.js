5fe36588fcb65a4a8b9c4256ace2d77c5eee6fa3
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-04-16 13:43:11 +0530

    Initial Update

--- 16_Promise.js
+++ 16_Promise.js
@@ -38,34 +38,34 @@
 
 // for Example : 
 
-function hallTicket(callback){
-    setTimeout(() => {
-        console.log("Hall ticket Received Successfully.");
-        callback();
-    },2000)
-}
+// function hallTicket(callback){
+//     setTimeout(() => {
+//         console.log("Hall ticket Received Successfully.");
+//         callback();
+//     },2000)
+// }
 
-function exam(callback){
-    setTimeout(() =>{
-        console.log("Exam Started");
-    },400)
+// function exam(callback){
+//     setTimeout(() =>{
+//         console.log("Exam Started");
+//     },400)
 
-    setTimeout(() =>{
-        console.log("Exam Finished");
-        callback();
-    },6000)
+//     setTimeout(() =>{
+//         console.log("Exam Finished");
+//         callback();
+//     },6000)
 
-}
+// }
 
-function result(){
-    console.log("Result Declared");
-}
+// function result(){
+//     console.log("Result Declared");
+// }
 
-hallTicket(()=>{
-    exam(() =>{
-        result();
-    })
-})
+// hallTicket(()=>{
+//     exam(() =>{
+//         result();
+//     })
+// })
 
 //===============================================
 
@@ -77,4 +77,178 @@ hallTicket(()=>{
 //  To avoid callback hell, we can use Promises or async/await.
 //==============================================
 
+
+// Promise : A Promise is an Object represents  the eventual complition (or failure) of an asynchronous operation
+// it allows you to attach handlers(like .then() and .catch()) to handle the result of an 
+// asynchronous operation
+
+//  Promise has a Three states : 
+
+// Pending : Pending is the initial state, in this state the promise is still being Processed.
+//  Fulfilled : Fulfilled is the state when the promise is resolved and the operation is completed.
+//  Rejected : Rejected is the state when the promise is rejected and the operation failed.
+
+
+// const promiseOne = new Promise (function (resolve, reject){
+//     // Do an Async Tasks...
+//     setTimeout(function(){
+//         console.log("Async task is complete");
+//         resolve();
+//     },1000)
+// });
+
+// promiseOne.then(function(){
+//     console.log("Promise is resolved");
+// })
+
+// =============================
+
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task 2.")
+//         resolve();
+//     },1000)
+// }).then(function(){
+//     console.log("Promise 2 is resolved");
+// })
+
+// ============================================
+
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username : "Venkatesh", email :'vpk@gmail.com'})
+//     },1000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user)
+// })
+
+// ==================
+
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false;
+//         if(!error){
+//             resolve({username : "Venkey", password : "vpk123"})
+//         }
+//         else{
+//             reject("Error : Something went Wrong.")
+//         }
+//     },2000)
+// })
+
+// promiseFour
+// .then((user) =>{
+//      console.log(user);
+//      return user.username;
+// })
+// .then((username) =>{
+//     console.log(username);
+// })
+// .catch((error) =>{
+//     console.log(error);
+// }).finally(() =>{
+//     console.log("Promise either resolve or rejected.");
+// })
+
+// ===============================================
+
+// const promiseFive = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true;
+//         if(!error){
+//             resolve({username : "JavaScript", password : "vpk123"})
+//         }
+//         else{
+//             reject("Error : JS went Wrong.")
+//         }
+//     },2000)
+// })
+
+
+
+// async function consumePromiseFive(){
+//     try{
+//         const response = await promiseFive;
+//         console.log(response)
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
+
+// consumePromiseFive();
+
+
+// ===================================
+
+// async function getAllUser(){
+//  try{
+//     const response =  await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await response.json();
+//     console.log(data);
+//  }catch(error){
+//     console.log(error);
+//  }
+// }
+// getAllUser();
+
+// =================================
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response)=>{
+//     return response.json();
+// })
+// .then((data) =>{
+//     console.log(data)
+// })
+// .catch((error) =>{
+//     console.log(error);
+// })
+
+
+// ========================================================
+
+// let promise = new Promise((resolve, reject) =>{
+//     let dataLoaded = false;
+
+//     setTimeout(()=>{
+//         if(dataLoaded){
+//             resolve("Data Loaded Successfully...");
+//         }
+//         else{
+//             reject("Failed to load Data...")
+//         }
+//     },3000)
+// })
+
+// promise
+// .then((result) =>{
+//     console.log(result);
+// })
+// .catch((error) =>{
+//     console.log(error);
+// })
+
+// ===================================
+
+let checkEvenOdd = (num)=>{
+  return  new Promise((resolve, reject)=>{
+        if(num % 2 === 0){
+            resolve("Number is Even");
+        }
+        else{
+            reject("Number is Odd");
+        }
+    })
+}
+
+checkEvenOdd(10)
+.then((result)=>{
+    console.log(result);
+})
+.catch((error) =>{
+    console.log("error");
+})
 
