709281e845739dc73816049582ab19de1dc76d22
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-09-28 09:36:57 +0530

    Operator revision completed

---
+++ 04-operators.js
@@ -0,0 +1,40 @@
+// What is Operators : 
+// Operators are special characters or symbols which is used to perform
+//  operations on Values and Variables
+
+// Operators are categorized into various types : 
+
+// 1. Arithmetic Operators : 
+//  Arithmetic operators  are used to perform mathematical operations like, 
+// addition , subtraction, multiplication , division, or modulus.
+
+let a = 22;
+let b = 45;
+
+console.log("Addition : ", a + b);
+console.log("Subtraction : ", a - b);
+console.log("Multiplication : ", a * b);
+console.log("Division : ",  b / a);
+console.log("Modulus : ", a % b);
+
+// 2. Comparison Operators : 
+//  Comparison operators are used to compare two values .
+//  >, <, >= , <= , != , == , ===
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b)
+console.log(a == b)
+console.log(a != b)
+console.log( a === b);
+
+
+// 3. Logical Operators : 
+//  Logical operators are used to combine or modify boolean expressions.
+//  &&, !, ||
+
+//  4. Assignment Operators : 
+//  Assignment Operators are used to assign values to the variables
+//  += ,-+ , =,
+
